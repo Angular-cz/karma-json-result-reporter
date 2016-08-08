a5fcf87433b467c184d2a47896c2cbff815f265d
You LIANG <yolian@microsoft.com>
2016-08-08 20:42:06 +0800

    change tests for using helper and logger

--- spec/jsonResultReporter.spec.js
+++ spec/jsonResultReporter.spec.js
@@ -1,5 +1,6 @@
 const os = require('os');
 const fs = require('fs');
+const mkdirp = require('mkdirp');
 const del = require('del');
 
 var reporterExport = require('./../index.js');
@@ -23,13 +24,29 @@ describe("JsonResultReporter", function() {
       this.formatError = jasmine.createSpy('formatError').and.callFake(function(error) {
         return error;
       });
+
+      this.helper = jasmine.createSpy('helper');
+      this.helper.mkdirIfNotExists = jasmine.createSpy('helper.mkdirIfNotExists spy').and.callFake(function(dirStr, callback) {
+        if (!fs.existsSync(dirStr)) {
+          mkdirp.sync(dirStr);
+        }
+        callback();
+      });
+
+      this.logger = jasmine.createSpy('logger');
+      log = jasmine.createSpy('log');
+      log.debug = jasmine.createSpy('log.debug spy').and.callFake(function(output) {});
+      log.warn = jasmine.createSpy('log.warn spy').and.callFake(function(output) {});
+      this.logger.create = jasmine.createSpy('logger.create spy').and.callFake(function(loggerName) {
+        return log;
+      });
     });
 
 
     it('should be instantiable', function() {
       var config = {};
 
-      var reporter = new JsonResultReporter(this.baseReporterDecorator, this.formatError, config);
+      var reporter = new JsonResultReporter(this.baseReporterDecorator, this.formatError, config, this.helper, this.logger);
 
       expect(reporter).toEqual(jasmine.any(Object));
       expect(this.baseReporterDecorator).toHaveBeenCalledTimes(1);
@@ -66,7 +83,7 @@ describe("JsonResultReporter", function() {
         };
 
 
-        var reporter = new JsonResultReporter(this.baseReporterDecorator, this.formatError, config);
+        var reporter = new JsonResultReporter(this.baseReporterDecorator, this.formatError, config, this.helper, this.logger);
         reporter.onBrowserError('test', ERROR_TEXT);
         reporter.onRunComplete();
 
@@ -78,7 +95,7 @@ describe("JsonResultReporter", function() {
           outputFile: this.tempTestDir + '/my/nested/path/report-file.json'
         };
 
-        var reporter = new JsonResultReporter(this.baseReporterDecorator, this.formatError, config);
+        var reporter = new JsonResultReporter(this.baseReporterDecorator, this.formatError, config, this.helper, this.logger);
         reporter.onBrowserError('test', ERROR_TEXT);
         reporter.onRunComplete();
 
